df1d5ba092da7ec162bce6173c339cf0fa27ddab
Farazzaidi22 <farazkhalid05@yahoo.com>
2021-12-13 02:51:42 +0500

    added firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {auth} from './firebase/firebase.utils'
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Routes>
-        <Route exact path='/' element={<HomePage/>}/>
-        <Route path='/shop' element={<ShopPage/>}/>
-        <Route path='/signin' element={<SignInAndSignUpPage/>}></Route>
-      </Routes>
-    </div>
-    
-  );
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  } 
+
+  unsubscribeFormAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFormAuth = auth.onAuthStateChanged( user => {
+      this.setState({ currentUser: user })
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFormAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Routes>
+          <Route exact path='/' element={<HomePage/>}/>
+          <Route path='/shop' element={<ShopPage/>}/>
+          <Route path='/signin' element={<SignInAndSignUpPage/>}></Route>
+        </Routes>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
